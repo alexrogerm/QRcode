16b7801e574b12a2a58aa32469ed65b5d1b03532
alexrogermoya <alex.roger01@estudiant.upf.edu>
2024-04-18 11:36:17 +0200

    changes

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <HeaderBody/>
+  <HelloWorld/>
+</template>
+
+<script>
+import HeaderBody from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBody,
+  }
+}
+</script>
+
+<style>
+* {
+    color: #00FFFF;
+}
+
+body{
+  background-color: #2c3e50;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" @click="showContent(item)" :key="item.id" :class="{ active: item === selectedItem }">{{
+        item.title
+      }}</li>
+    </ul>
+    <div class="content" v-show="selectedItem === items[0]">
+      <ContentOne v-on:input-value="updateValue" />
+    </div>
+    <div class="content" v-show="selectedItem === items[1]">
+      <ContentTwo v-on:input-value2="updateValue2" v-on:input-value3="updateValue3" v-on:input-value4="updateValue4"
+        v-on:input-value5="updateValue5" />
+    </div>
+    <div class="content" v-show="selectedItem === items[2]">
+      <ContentThree @selected="setSelectedImage" />
+    </div>
+    <div class="content" v-show="selectedItem === items[3]">
+      <DisplayArea :InputData="InputData" :InputData2="InputData2" :InputData3="InputData3" :hexCode="hexCode"
+:hexCode2="hexCode2" :selectedImage="selectedImage" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ContentOne from './Content/ContentOne.vue';
+import ContentTwo from './Content/ContentTwo.vue';
+import ContentThree from './Content/ContentThree.vue';
+import DisplayArea from './Content/DisplayArea.vue';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    ContentOne,
+    ContentTwo,
+    ContentThree,
+    DisplayArea
+  },
+  data() {
+    return {
+      InputData: '',
+      hexCode: '',
+      hexCode2: '',
+      InputData2: '',
+      InputData3: '',
+      selectedImage: '',
+      items: [
+        { title: 'ENTER CONTENT' },
+        { title: 'CUSTOMIZE' },
+        { title: 'ADD LOGO' },
+        { title: 'DISPLAY' },
+      ],
+      selectedItem: null
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.InputData = value
+    },
+    updateValue2(value) {
+      this.hexCode = value
+    },
+    updateValue3(value) {
+      this.hexCode2 = value
+    },
+    updateValue4(value) {
+      this.InputData2 = value
+    },
+    updateValue5(value) {
+      this.InputData3 = value
+    },
+    setSelectedImage(selectedImage) {
+      this.selectedImage = selectedImage;
+    },
+    showContent(item) {
+      this.selectedItem = item
+    },
+  },
+  emits: ['Selected']
+}
+</script>
+
+<style scoped>
+ul {
+  margin: 5% 5% 2% 5%;
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  padding: 25px 0;
+  border-radius: 25px;
+  border: 0.1px solid #099FFF;
+  background-color: #18181B;
+  box-shadow: 0 4px 8px 0 #00FFFF, 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+li {
+  margin: auto;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+li::after {
+  display: block;
+  content: '';
+  border-bottom: solid 1px #00FFFF;
+  padding-bottom: 1%;
+  transform: scaleX(0);
+  transition: transform 300ms ease-in-out;
+}
+
+li:hover::after {
+  transform: scaleX(1);
+}
+
+.content {
+  border-radius: 25px;
+  margin: 0 5% 0 5%;
+  padding: 25px 0;
+  background: #2B3241;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: center;
+}
+</style>
